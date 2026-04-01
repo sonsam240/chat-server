0036da5cb782e241820e27b13bfa5efb09b4927c
sonsam240 <sonsam240@gmail.com>
2026-04-01 18:33:35 +1000

    file storage

--- server.js
+++ server.js
@@ -7,32 +7,41 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, {cors:{origin:"*"}});
 
-let users = {}; // userId -> socket.id
+const io = new Server(server, { cors: { origin: "*" } });
 
-io.on("connection", socket=>{
+// Словарь пользователей: userId -> socket.id
+let users = {};
+
+io.on("connection", socket => {
   console.log("user connected");
 
-  socket.on("register", userId=>{
-    users[userId]=socket.id;
+  // Регистрация пользователя
+  socket.on("register", userId => {
+    users[userId] = socket.id;
     console.log("registered:", userId);
   });
 
-  socket.on("privateMessage", msg=>{
-    // отправить получателю
-    const target = users[msg.to];
-    if(target) io.to(target).emit("newMessage", msg);
-
-    // отправителю тоже можно отправить обратно, но на фронте мы это не дублируем
+  // Личные сообщения
+  socket.on("privateMessage", msg => {
+    const targetSocket = users[msg.to];
+    if (targetSocket) {
+      io.to(targetSocket).emit("newMessage", msg);
+    }
+    // отправителю тоже показываем сообщение
     socket.emit("newMessage", msg);
   });
 
-  socket.on("disconnect", ()=>{
-    for(let id in users) if(users[id]===socket.id) delete users[id];
+  socket.on("disconnect", () => {
+    for (let id in users) {
+      if (users[id] === socket.id) {
+        delete users[id];
+        break;
+      }
+    }
     console.log("user disconnected");
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, ()=>console.log("Server started on port "+PORT));
+server.listen(PORT, () => console.log("Server started on port " + PORT));
